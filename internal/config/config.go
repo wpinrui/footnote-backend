481c43a75dec1a416f1e35281bc3dae608263aa1
Ivan <77185900+wpinrui@users.noreply.github.com>
2025-06-05 14:23:13 +0800

    Refactor database connection logic in NewConfig function; simplify DSN construction for local environment

--- internal/config/config.go
+++ internal/config/config.go
@@ -15,24 +15,13 @@ func NewConfig() *Config {
 	env := os.Getenv("ENV") // e.g. "local" or "cloud"
 
 	var dsn string
-	if env == "local" {
-		dbHost := os.Getenv("DB_HOST")
-		dbPort := os.Getenv("DB_PORT")
-		dbUser := os.Getenv("DB_USER")
-		dbPassword := os.Getenv("DB_PASS")
-		dbName := os.Getenv("DB_NAME")
-		dsn = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-			dbHost, dbPort, dbUser, dbPassword, dbName)
-	} else {
-		// Use Cloud SQL via Unix socket
-		dbUser := os.Getenv("DB_USER")
-		dbPassword := os.Getenv("DB_PASS")
-		dbName := os.Getenv("DB_NAME")
-		instanceConnName := os.Getenv("INSTANCE_CONNECTION_NAME")
-
-		dsn = fmt.Sprintf("host=/cloudsql/%s user=%s password=%s dbname=%s sslmode=disable",
-			instanceConnName, dbUser, dbPassword, dbName)
-	}
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASS")
+	dbName := os.Getenv("DB_NAME")
+	dsn = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
 
 	return &Config{
 		Dsn:       dsn,
